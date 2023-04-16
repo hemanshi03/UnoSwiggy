417cd92c3e5dd3ce0b7ec6f80676bd6329363d37
Hemashi Maheta <Hemashi.Maheta@gmail.com>
2023-04-16 16:49:55 +0530

    Added new tests

--- TestCustomUno.java
+++ TestCustomUno.java
@@ -37,17 +37,35 @@ public class TestCustomUno {
     return false;
   }
 
-  boolean testNumOfCardsDealtInitially(
+  boolean testNumOfCardsDealtInitially(int numberOfCardsToDealInitially) {
+    for (int i = 0; i < this.newGame.players.length; i++) {
+      Player p = this.newGame.players[i];
+      if (p.getCardSize() != numberOfCardsToDealInitially) return false;
+    }
+    return true;
+  }
+
+  boolean testDeckContainsCorrectNumberOfCards(
     int numOfPlayer,
-    int numberOfCardsToDealInitially,
-    Game newGame
+    int numberOfCardsToDealInitially
   ) {
-    TestCustomUno test = new TestCustomUno(numOfPlayer, numOfPlayer);
-    return false;
+    int expected_number_of_cards_after_dealing =
+      52 - (numOfPlayer * numberOfCardsToDealInitially);
+    this.newGame.deck.initialize();
+    this.newGame.deck.shuffle();
+    this.newGame.initialDeal(numberOfCardsToDealInitially);
+    return (
+        this.newGame.deck.deck.size() == expected_number_of_cards_after_dealing
+      )
+      ? true
+      : false;
   }
 
   public static void main(String[] args) {
-
+    /*
+     * Test 1
+     * Negative Test to check number of players allocated correctly
+     */
     System.out.println(
       "Negative Test to check number of players allocated correctly"
     );
@@ -55,12 +73,43 @@ public class TestCustomUno {
     assertTest(!testNumOfPlayersSetCorrectly.testNumOfPlayersSet(3));
     testNumOfPlayersSetCorrectly = null;
 
+    /*
+     * Test 2
+     * Positive Test to check number of players allocated correctly
+     */
     System.out.println(
       "Positive Test to check number of players allocated correctly"
     );
     testNumOfPlayersSetCorrectly = new TestCustomUno(4, 2);
     assertTest(testNumOfPlayersSetCorrectly.testNumOfPlayersSet(4));
+    testNumOfPlayersSetCorrectly = null;
 
+    /*
+     *
+     * Test 3
+     * Test to check Every player is dealt x cards"
+     */
+    System.out.println("Test to check Every player is dealt x cards");
+    testNumOfPlayersSetCorrectly = new TestCustomUno(4, 2);
+    assertTest(testNumOfPlayersSetCorrectly.testNumOfCardsDealtInitially(2));
+    testNumOfPlayersSetCorrectly = null;
 
+    /*
+     *
+     * Test 4
+     * Test to check deck contains correct number of cards after dealing
+     * to players
+     */
+    System.out.println(
+      "Test to check deck contains correct number of cards after dealing to players"
+    );
+    testNumOfPlayersSetCorrectly = new TestCustomUno(4, 2);
+    assertTest(
+      testNumOfPlayersSetCorrectly.testDeckContainsCorrectNumberOfCards(4, 2)
+    );
+    /*
+     *
+     *
+     */
   }
 }
